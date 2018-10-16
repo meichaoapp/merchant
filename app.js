d5eb87089e02f66aba955c84da174041ff2d76fd
bjsonghongxu <bjsonghongxu@corp.netease.com>
2018-10-16 19:26:56 +0800

    添加组件

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
+import Wux from 'components/wux'
+import WxValidate from 'assets/plugins/WxValidate'
 var util = require('./utils/util.js');
 var api = require('./config/api.js');
 var user = require('./services/user.js');
 
 App({
   onLaunch: function () {
-    //获取用户的登录信息
-    user.checkLogin().then(res => {
-      console.log('app login')
-      this.globalData.userInfo = wx.getStorageSync('userInfo');
-      this.globalData.token = wx.getStorageSync('token');
-    }).catch(() => {
-      
-    });
   },
-  
+  Wux: Wux,
+  WxValidate: (rules, messages) => new WxValidate(rules, messages), 
+
   globalData: {
     _version:"v1.0",
     _platformName:"美超",
