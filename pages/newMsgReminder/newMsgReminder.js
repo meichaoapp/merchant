6c7026a0e1722afbb8624d027eefc8b2e4bd8605
songhx <sleeponthestar@163.com>
2019-01-06 23:23:54 +0800

    消息提醒

--- pages/newMsgReminder/newMsgReminder.js
+++ pages/newMsgReminder/newMsgReminder.js
@@ -8,7 +8,7 @@ Page({
         isOpen: false,//播放开关
         starttime: '00:00', //正在播放时长
         duration: '06:41',   //总时长
-        src: "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46"
+      src: "https://wxpic.iliangpin.cn/meichao/message_reminding.mp3"
     },
     // 播放
     listenerButtonPlay: function () {
